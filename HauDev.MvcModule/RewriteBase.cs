d43e041183a4550b6e9600b84d6f4ece93c16ddb
haudevpro <haudevpro@gmail.com>
2016-04-03 01:14:13 +0700

    a

---
+++ HauDev.MvcModule/RewriteBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.SessionState;
+
+namespace HauDev.MvcModule
+{
+    public class RewriteBase : IHttpHandlerFactory, IRequiresSessionState
+    {
+        public IHttpHandler GetHandler(HttpContext context, string requestType, string url, string pathTranslated)
+        {
+            return context.Handler;
+        }
+
+        public void ReleaseHandler(IHttpHandler handler)
+        {
+           
+        }
+    }
+}
